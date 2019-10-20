3d472266e34cb9ad4b2b9e4fe281b79e3605ddac
noahspaceapps2019 <56760186+noahspaceapps2019@users.noreply.github.com>
2019-10-20 13:42:39 +0200

    delete_drown.js

--- Drown.js
+++
@@ -1,9 +0,0 @@
-class Drown extends Phaser.Scene {
-  constructor() {
-    super("Drown.death");
-  }
-
-  create() {
-      this.add.text(20, 20, "Loading game...", { font: "25px Arial", fill: "yellow" });
-  }
-}
